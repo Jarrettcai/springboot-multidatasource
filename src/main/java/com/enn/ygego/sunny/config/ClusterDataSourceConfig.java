8c3153f26295bff96cb4af3e1dbe1f3ad04dcd5e
Jarrettcai <380257712@qq.com>
2019-11-13 22:18:31 +0800

    对项目做了一些修改，使他能够跑起来

--- src/main/java/com/enn/ygego/sunny/config/ClusterDataSourceConfig.java
+++ src/main/java/com/enn/ygego/sunny/config/ClusterDataSourceConfig.java
@@ -1,6 +1,11 @@
 package com.enn.ygego.sunny.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -8,67 +13,139 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.github.pagehelper.PageInterceptor;
 
+/**
+ * 
+* @Title: ClusterDataSourceConfig
+* @Description: 
+*  另一个数据源配置
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年4月26日
+ */
 @Configuration
-@MapperScan(basePackages = ClusterDataSourceConfig.PACKAGE,sqlSessionFactoryRef = "clusterDataSource")
+@MapperScan(basePackages = ClusterDataSourceConfig.PACKAGE, sqlSessionFactoryRef = "clusterSqlSessionFactory")
 public class ClusterDataSourceConfig {
-    static  final String PACKAGE="com.enn.ygego.sunny.dao.cluster";
-    static final String MAPPER_LOCATION="classpath:mapper/cluster/*.xml";
-    @Value("${cluster.datasource.url}")
-    private String url;
 
-    @Value("${cluster.datasource.username}")
-    private String username;
+ static final String PACKAGE = "com.pancm.dao.cluster";
+ static final String MAPPER_LOCATION = "classpath:mapper/cluster/*.xml";
 
-    @Value("${cluster.datasource.password}")
-    private String password;
+ @Value("${cluster.datasource.url}")
+ private String url;
 
-    @Value("${cluster.datasource.driverClassName}")
-    private String driverClassName;
+ @Value("${cluster.datasource.username}")
+ private String username;
 
-    @Value("${spring.datasource.filters}")
-    private String filters;
+ @Value("${cluster.datasource.password}")
+ private String password;
 
-    @Value("${spring.datasource.connectionProperties}")
-    private String connectionProperties;
+ @Value("${cluster.datasource.driverClassName}")
+ private String driverClass;
 
-    @Bean(name = "clusterDataSource")
-    @Primary
-    public DataSource clusterDataSource(){
-        DruidDataSource dataSource=new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setDriverClassName(driverClassName);
-        try {
-            dataSource.setFilters(filters);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        dataSource.setConnectionProperties(connectionProperties);
-        return dataSource;
-    }
+ 
+ @Value("${spring.datasource.initialSize}")  
+ private int initialSize;  
+   
+ @Value("${spring.datasource.minIdle}")  
+ private int minIdle;  
+   
+ @Value("${spring.datasource.maxActive}")  
+ private int maxActive;  
+   
+ @Value("${spring.datasource.maxWait}")  
+ private int maxWait;  
+   
+ @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")  
+ private int timeBetweenEvictionRunsMillis;  
+   
+ @Value("${spring.datasource.minEvictableIdleTimeMillis}")  
+ private int minEvictableIdleTimeMillis;  
+   
+ @Value("${spring.datasource.validationQuery}")  
+ private String validationQuery;  
+   
+ @Value("${spring.datasource.testWhileIdle}")  
+ private boolean testWhileIdle;  
+   
+ @Value("${spring.datasource.testOnBorrow}")  
+ private boolean testOnBorrow;  
+   
+ @Value("${spring.datasource.testOnReturn}")  
+ private boolean testOnReturn;  
+   
+ @Value("${spring.datasource.poolPreparedStatements}")  
+ private boolean poolPreparedStatements;  
+   
+ @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")  
+ private int maxPoolPreparedStatementPerConnectionSize;  
+   
+ @Value("${spring.datasource.filters}")  
+ private String filters;  
+   
+ @Value("{spring.datasource.connectionProperties}")  
+ private String connectionProperties;
+ 
+ @Bean(name = "clusterDataSource")
+ public DataSource clusterDataSource() {
+     DruidDataSource dataSource = new DruidDataSource();
+     dataSource.setUrl(url);  
+     dataSource.setUsername(username);  
+     dataSource.setPassword(password);  
+     dataSource.setDriverClassName(driverClass);  
+       
+     //具体配置 
+     dataSource.setInitialSize(initialSize);  
+     dataSource.setMinIdle(minIdle);  
+     dataSource.setMaxActive(maxActive);  
+     dataSource.setMaxWait(maxWait);  
+     dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);  
+     dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);  
+     dataSource.setValidationQuery(validationQuery);  
+     dataSource.setTestWhileIdle(testWhileIdle);  
+     dataSource.setTestOnBorrow(testOnBorrow);  
+     dataSource.setTestOnReturn(testOnReturn);  
+     dataSource.setPoolPreparedStatements(poolPreparedStatements);  
+     dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);  
+     try {  
+         dataSource.setFilters(filters);  
+     } catch (SQLException e) { 
+     	e.printStackTrace();
+     }  
+     dataSource.setConnectionProperties(connectionProperties);  
+     return dataSource;
+ }
 
-    @Bean(name = "clusterTransactionManager")
-    @Primary
-    public DataSourceTransactionManager clusterTransactionManager(){
-        return  new DataSourceTransactionManager(clusterDataSource());
-    }
-    @Bean(name = "clusterSqlSessionFactory")
-    @Primary
-    public SqlSessionFactory clusterSqlSessionFactory(@Qualifier("clusterDataSource") DataSource masterDataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory=new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(masterDataSource);
-        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(ClusterDataSourceConfig.MAPPER_LOCATION));
-        return sessionFactory.getObject();
-    }
+ @Bean(name = "clusterTransactionManager")
+ public DataSourceTransactionManager clusterTransactionManager() {
+     return new DataSourceTransactionManager(clusterDataSource());
+ }
 
-
-
+ @Bean(name = "clusterSqlSessionFactory")
+ public SqlSessionFactory clusterSqlSessionFactory(@Qualifier("clusterDataSource") DataSource clusterDataSource)
+         throws Exception {
+     final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+     sessionFactory.setDataSource(clusterDataSource);
+     //分页插件
+     Interceptor interceptor = new PageInterceptor();
+     Properties properties = new Properties();
+     //数据库
+     properties.setProperty("helperDialect", "mysql");
+     //是否将参数offset作为PageNum使用
+     properties.setProperty("offsetAsPageNum", "true");
+     //是否进行count查询
+     properties.setProperty("rowBoundsWithCount", "true");
+     //是否分页合理化
+     properties.setProperty("reasonable", "false");
+     interceptor.setProperties(properties);
+     sessionFactory.setPlugins(new Interceptor[] {interceptor});
+     
+     
+     sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(ClusterDataSourceConfig.MAPPER_LOCATION));
+     return sessionFactory.getObject();
+ }
 }
